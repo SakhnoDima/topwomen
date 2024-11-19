2bebf12793a10203870b491f6e8e060d935aa157
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2024-11-19 12:53:47 +0200

    Update vinci.js

--- crawlers/vinci.js
+++ crawlers/vinci.js
@@ -110,7 +110,6 @@ async function fetchAllJobResponses(page, offset) {
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
         "Cache-Control": "no-cache",
       },
-      timeout: 10000,
     });
 
     return response.data.results;
