e5f2d54f159e0460d7c3d9ce57bf82a9e179ff1b
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2025-06-26 15:18:19 +0300

    Remove .env

--- mixpanel.js
+++ mixpanel.js
@@ -5,29 +5,32 @@ dotenv.config();
 import Mixpanel from "mixpanel";
 import { getTime } from "./assistants/helpers.js";
 
-const mixpanel = Mixpanel.init("1bd22151e54aec8e76f52e4b74e3580e", {
-  secret: "0cdc889d1420b2d13e90e2d563d09a89",
+const MIXPANEL_SECRET = process.env.MIXPANEL_SECRET;
+const MIXPANEL_TOKEN = process.env.MIXPANEL_TOKEN;
+
+const mixpanel = Mixpanel.init(MIXPANEL_TOKEN, {
+    secret: MIXPANEL_SECRET,
 });
 
 export const trackMixpanel = async (
-  crawler,
-  totalVacancies,
-  successful,
-  message
-) => {
-  mixpanel.track(
     crawler,
-    {
-      "Vacancies found": totalVacancies,
-      Status: successful === true ? "Success" : "Fail",
-      Message: message ? message : "Parsing completed successfully",
-    },
-    (err) => {
-      if (err) {
-        console.error(`Error tracking event for ${crawler}:`, err);
-      } else {
-        console.log(`Event for ${crawler} tracked successfully`);
-      }
-    }
-  );
+    totalVacancies,
+    successful,
+    message
+) => {
+    mixpanel.track(
+        crawler,
+        {
+            "Vacancies found": totalVacancies,
+            Status: successful === true ? "Success" : "Fail",
+            Message: message ? message : "Parsing completed successfully",
+        },
+        (err) => {
+            if (err) {
+                console.error(`Error tracking event for ${crawler}:`, err);
+            } else {
+                console.log(`Event for ${crawler} tracked successfully`);
+            }
+        }
+    );
 };
