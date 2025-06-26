74ce98cd6a6c55e61165f82144e55fa109b60a06
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2025-06-26 14:58:32 +0300

    Remove .env and node_modules

--- controllers/dataControllers.js
+++ controllers/dataControllers.js
@@ -3,44 +3,44 @@ import { trackMixpanel } from "../mixpanel.js";
 import axiosRetry from "axios-retry";
 
 axiosRetry(axios, {
-  retries: 5, // Кількість спроб
-  retryDelay: (retryCount) => {
-    console.log(`Retry saving data: ${retryCount}`);
-    return retryCount * 2000; // Затримка між спробами (1 секунда, 2 секунди, 3 секунди тощо)
-  },
-  retryCondition: (error) => {
-    const isRetryable = axiosRetry.isNetworkOrIdempotentRequestError(error);
-    return isRetryable || [502, 503, 504].includes(error.response?.status);
-  },
+    retries: 5, // Кількість спроб
+    retryDelay: (retryCount) => {
+        console.log(`Retry saving data: ${retryCount}`);
+        return retryCount * 2000; // Затримка між спробами (1 секунда, 2 секунди, 3 секунди тощо)
+    },
+    retryCondition: (error) => {
+        const isRetryable = axiosRetry.isNetworkOrIdempotentRequestError(error);
+        return isRetryable || [502, 503, 504].includes(error.response?.status);
+    },
 });
 
 export const dataSaver = async (companyName, vacancies) => {
-  const responseBody = {
-    company: companyName,
-    vacancies,
-  };
+    const responseBody = {
+        company: companyName,
+        vacancies,
+    };
 
-  try {
-    await axios.post(
-      "https://topwomen.careers/wp-json/custom/v1/add-company-vacancies",
-      JSON.stringify(responseBody),
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    try {
+        await axios.post(
+            "https://topwomen.careers/wp-json/custom/v1/add-company-vacancies",
+            JSON.stringify(responseBody),
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        );
 
-    console.log(`${companyName} vacancies saved!`);
-    console.log(`Total vacancies in ${companyName}`, vacancies.length);
-    trackMixpanel(companyName, vacancies.length, true);
-  } catch (error) {
-    console.error("Error saving data:", error.message);
-    console.error("Error saving data:", {
-      message: error.message,
-      response: error.response?.data || "No response data",
-      status: error.response?.status || "No status",
-    });
-    throw new Error(`Saving data failed: ${error.message}`);
-  }
+        console.log(`${companyName} vacancies saved!`);
+        console.log(`Total vacancies in ${companyName}`, vacancies.length);
+        trackMixpanel(companyName, vacancies.length, true);
+    } catch (error) {
+        console.error("Error saving data:", error.message);
+        console.error("Error saving data:", {
+            message: error.message,
+            response: error.response?.data || "No response data",
+            status: error.response?.status || "No status",
+        });
+        throw new Error(`Saving data failed: ${error.message}`);
+    }
 };
--- crawlers/siemens.js
+++ crawlers/siemens.js
@@ -7,67 +7,70 @@ import { delayer } from "../assistants/helpers.js";
 import { getEnglishCountryName } from "../helpers/index.js";
 
 export async function fetchingDataFromSiemens() {
-  const limit = 10;
-  const parallelRequests = 5;
-  let page = 0;
-  let results = [];
-  console.log("Siemens crawler started");
-  try {
-    while (true) {
-      const positions = await fetchBatch(page, parallelRequests, limit);
-      for (const position of positions) {
-        const title = position.name.trim();
-        const location = position.location.split(",")[2].trim();
-        const url = position.canonicalPositionUrl;
+    const limit = 10;
+    const parallelRequests = 5;
+    let page = 0;
+    let results = [];
+    console.log("Siemens crawler started");
+    try {
+        while (true) {
+            const positions = await fetchBatch(page, parallelRequests, limit);
+            for (const position of positions) {
+                const title = position.name.trim();
+                const location = position.location.split(",")[2].trim();
+                const url = position.canonicalPositionUrl;
 
-        results.push({
-          title,
-          sector: await getSector(title),
-          location: getEnglishCountryName(location),
-          url,
-        });
-      }
+                results.push({
+                    title,
+                    sector: await getSector(title),
+                    location: getEnglishCountryName(location),
+                    url,
+                });
+            }
 
-      if (positions.length === 0) {
-        break;
-      }
-      page += parallelRequests;
-      await delayer(5000);
+            if (positions.length === 0) {
+                break;
+            }
+            page += parallelRequests;
+            await delayer(1000);
+        }
+        await dataSaver("Siemens", results);
+    } catch (error) {
+        await trackMixpanel("Siemens", 0, false, error.message);
+        console.error("Siemens crawler error:", error);
     }
-    await dataSaver("Siemens", results);
-  } catch (error) {
-    await trackMixpanel("Siemens", 0, false, error.message);
-    console.error("Siemens crawler error:", error);
-  }
 }
 async function fetchBatch(page, parallelRequests, limit) {
-  for (let attempt = 1; attempt <= 5; attempt++) {
-    try {
-      const promises = Array.from({ length: parallelRequests }, (_, i) => {
-        const offset = (page + i) * limit;
-        return axios
-          .get(
-            `https://jobs.siemens.com/api/apply/v2/jobs?domain=siemens.com&start=${offset}`
-          )
-          .then((response) => response.data.positions);
-      });
+    for (let attempt = 1; attempt <= 5; attempt++) {
+        try {
+            const promises = Array.from(
+                { length: parallelRequests },
+                (_, i) => {
+                    const offset = (page + i) * limit;
+                    return axios
+                        .get(
+                            `https://jobs.siemens.com/api/apply/v2/jobs?domain=siemens.com&start=${offset}`
+                        )
+                        .then((response) => response.data.positions);
+                }
+            );
 
-      const responses = await Promise.all(promises);
-      return responses.flat();
-    } catch (error) {
-      if (
-        error.response &&
-        (error.response.status === 429 || error.response.status === 403)
-      ) {
-        console.warn(
-          `Received ${error.response.status}. Attempt ${attempt}. Waiting for 90000ms...`
-        );
-        await delayer(90000);
-      } else {
-        throw error;
-      }
+            const responses = await Promise.all(promises);
+            return responses.flat();
+        } catch (error) {
+            if (
+                error.response &&
+                (error.response.status === 429 || error.response.status === 403)
+            ) {
+                console.warn(
+                    `Received ${error.response.status}. Attempt ${attempt}. Waiting for 90000ms...`
+                );
+                await delayer(90000);
+            } else {
+                throw error;
+            }
+        }
     }
-  }
 
-  throw new Error("Failed to fetch batch after 5 attempts.");
+    throw new Error("Failed to fetch batch after 5 attempts.");
 }
